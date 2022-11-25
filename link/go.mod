a37af3dce051065bd4ff245a02a03cff7414fcac
Kunju Perath <kperath@gmail.com>
2022-11-25 15:13:59 -0500

    htmlParser

---
+++ link/go.mod
@@ -0,0 +1,5 @@
+module github.com/kperath/gophercises/link
+
+go 1.18
+
+require golang.org/x/net v0.2.0 // indirect
---
+++ link/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getLinkText(node *html.Node, linkText *string) {
+	if node.Type == html.TextNode {
+		*linkText += node.Data
+	}
+	for sib := node.FirstChild; sib != nil; sib = sib.NextSibling {
+		getLinkText(sib, linkText)
+	}
+}
+
+func addLink(node *html.Node) (link Link) {
+	for _, a := range node.Attr {
+		if a.Key == "href" {
+			link.Href = a.Val
+			break
+		}
+	}
+	getLinkText(node, &link.Text)
+	return link
+}
+
+func traverse(node *html.Node, links *[]Link) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		*links = append(*links, addLink(node))
+	}
+	for sib := node.FirstChild; sib != nil; sib = sib.NextSibling {
+		traverse(sib, links)
+	}
+}
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func getLinks(filename string) []Link {
+	f, err := os.Open(filename)
+	check(err)
+	r := bufio.NewReader(f)
+	node, err := html.Parse(r)
+	check(err)
+	var links []Link
+	traverse(node, &links)
+	return links
+}
+
+func main() {
+	fmt.Println("==HTML PARSER==")
+	fmt.Println(getLinks("ex2.html"))
+}
