3aed54fb46f55fa17b799f1e28c17bd4f30c3aaa
Kunju Perath <kperath@gmail.com>
2022-11-27 22:35:25 -0500

    bug

--- sitebuilder/main.go
+++ sitebuilder/main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
-
-var domain string = "https://kperath.com"
-
-func normalizeDomain(domain string) string {
-	prefixes := []string{"https://", "http://", "www."}
-	for _, p := range prefixes {
-		domain = strings.TrimPrefix(domain, p)
-	}
-	return domain
-}
 
 func check(err error) {
 	if err != nil {
@@ -25,26 +17,67 @@ func check(err error) {
 	}
 }
 
-func getLinks(node *html.Node) {
+// www.s.com/a/b/c/a
+// {s.com/, s.com/a, s.com/a/b, s.com/a/b/c}
+
+type Set map[string]struct{}
+
+func (s Set) Add(k string) {
+	s[k] = struct{}{}
+}
+
+func getLinks(node *html.Node, currentPath string, baseURL *url.URL, visited Set, depth int) {
+	if depth == 0 {
+		return
+	}
+
 	if node.Type == html.ElementNode && node.Data == "a" {
 		// found link
 		for _, a := range node.Attr {
 			if a.Key == "href" {
+				link, err := url.Parse(a.Val)
+				if err != nil {
+					break
+				}
+				if link.Host != "" && link.Host != baseURL.Host {
+					break
+				}
+
+				if _, ok := visited[currentPath+link.Path]; ok {
+					break
+				}
+
+				nextLink := currentPath + link.Path
+				resp, err := http.Get(nextLink)
+				check(err)
+				defer resp.Body.Close()
+				data, err := io.ReadAll(resp.Body)
+				check(err)
+				doc, err := html.Parse(strings.NewReader(string(data)))
+				check(err)
+				visited.Add(nextLink)
+				fmt.Println(visited)
+				getLinks(doc, nextLink, baseURL, visited, depth-1)
+				break
 			}
 		}
 	}
 	for sib := node.FirstChild; sib != nil; sib = sib.NextSibling {
-		getLinks(node)
+		getLinks(sib, currentPath, baseURL, visited, depth-1)
 	}
 }
 
 func main() {
+	var domain string = "https://kperath.com"
 	resp, err := http.Get(domain)
 	check(err)
 	defer resp.Body.Close()
-	data, err := io.ReadAll(resp.Body)
+	doc, err := html.Parse(resp.Body)
 	check(err)
-	doc, err := html.Parse(strings.NewReader(string(data)))
+	u, err := url.Parse(domain)
 	check(err)
-	getLinks(doc)
+
+	s := make(Set)
+	getLinks(doc.FirstChild, u.Host, u, s, 2)
+	fmt.Println(s)
 }
